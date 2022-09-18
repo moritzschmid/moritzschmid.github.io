763417ee81a1e53b56381c7624a9ddcbc6039669
mo <mo@moritzschmmid.de>
2022-09-18 13:38:22 +0200

    add Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Header.css';
+
+const Header = () => (
+  <div className="Header" data-testid="Header">
+    moritzschmid.github.io
+  </div>
+);
+
+Header.propTypes = {};
+
+Header.defaultProps = {};
+
+export default Header;
