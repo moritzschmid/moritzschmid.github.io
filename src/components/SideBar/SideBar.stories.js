c9f1b8e861f5ed8cf97612cb46b62edb38baf5fe
moritzschmid <moe@moes.de>
2023-04-08 18:11:51 +0200

    remove storyrouter

--- src/components/SideBar/SideBar.stories.js
+++ src/components/SideBar/SideBar.stories.js
@@ -1,12 +1,37 @@
 /* eslint-disable */
+
 import SideBar from './SideBar';
+
+
+
+import { storiesOf } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
+
+
+storiesOf('SideBar', module)
+  .addDecorator(StoryRouter())
+  .add('sideBar', () => (
+    <SideBar />
+  ));
 
 export default {
   title: "SideBar",
+
+  component: SideBar,
 };
 
 export const Default = () => <SideBar />;
 
+
 Default.story = {
   name: 'default',
+  parameters: {
+    reactRouter: {
+      routePath: '/pong',
+      //routeParams: { userId: '42' },
+      routeHandle: "Profile",
+      //  searchParams: { tab: 'activityLog' },
+      routeState: { fromPage: 'homePage' },
+    }
+  }
 };
